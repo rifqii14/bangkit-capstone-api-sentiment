4f0b75ea983034c12dcbd0366a049a7f0d5581c7
Muhammad Rifqi Maulatur Rahman <rifqimaulatur.muhammad@gmail.com>
2021-05-30 18:02:46 +0700

    change model structure from ML path

--- Model.py
+++ Model.py
@@ -35,7 +35,76 @@ df.dtypes
 
 print(df[df["label"] == 0].count(), df[df["label"] == 1].count(), df[df["label"] == 2].count())
 
+# store in a dataset and random shuffle
+dataset = df.values
+np.random.shuffle(dataset)
 
+# split train dev test
+TRAIN_SIZE = round(len(dataset) * 0.7)
+VAL_SIZE = round(len(dataset) * 0.2)
+TEST_SIZE = len(dataset) - TRAIN_SIZE - VAL_SIZE
+
+print("trainsize {}, val size {}, test size {}".format(TRAIN_SIZE, VAL_SIZE, TEST_SIZE))
+
+# Each tweet convert to words and store in corpus
+train_dataset = dataset[:TRAIN_SIZE]
+val_dataset = dataset[TRAIN_SIZE: (TRAIN_SIZE+VAL_SIZE)]
+test_dataset = dataset[(VAL_SIZE+ TRAIN_SIZE):]
+print(len(train_dataset), len(val_dataset), len(test_dataset))
+train_dataset
+
+# train data and label
+X_train = train_dataset[:, 0]
+Y_train = train_dataset[:, 1]
+
+# validation data and label
+X_validation = val_dataset[:, 0]
+Y_validation = val_dataset[:, 1]
+
+# test data and label
+X_test = test_dataset[:, 0]
+Y_test =test_dataset[:, 1]
+
+# Tokenizer
+max_words = 5000
+max_len = 100
+
+tokenizer = Tokenizer(num_words=max_words, oov_token='<OOV>')
+tokenizer.fit_on_texts(X_train)
+sequences = tokenizer.texts_to_sequences(X_train)
+X_train = pad_sequences(sequences, maxlen=max_len)
+val_sequences = tokenizer.texts_to_sequences(X_validation)
+X_validation = pad_sequences(val_sequences, maxlen=max_len)
+
+# One hot encode labels
+Y_train=np.asarray(Y_train).astype(np.int)
+Y_validation = np.asarray(Y_validation).astype(np.int)
+
+temp_train = []
+for val in Y_train:
+  if val == 0:
+    temp_train.append([1, 0, 0])
+  if val == 1:
+    temp_train.append([0, 1, 0])
+  if val == 2:
+    temp_train.append([0, 0, 1])
+
+Y_train = temp_train
+
+temp_val = []
+for val in Y_validation:
+  if val == 0:
+    temp_val.append([1, 0, 0])
+  if val == 1:
+    temp_val.append([0, 1, 0])
+  if val == 2:
+    temp_val.append([0, 0, 1])
+
+Y_validation = temp_val
+
+Y_train=np.asarray(Y_train).astype(np.int)
+Y_validation = np.asarray(Y_validation).astype(np.int)
+        
 class SentimentText(BaseModel):
     text_twitter: str
         
@@ -48,76 +117,6 @@ class SentimentModel:
             self.model = self._train_model()
             
     def _train_model(self):
-        # store in a dataset and random shuffle
-        dataset = df.values
-        np.random.shuffle(dataset)
-
-        # split train dev test
-        TRAIN_SIZE = round(len(dataset) * 0.7)
-        VAL_SIZE = round(len(dataset) * 0.2)
-        TEST_SIZE = len(dataset) - TRAIN_SIZE - VAL_SIZE
-
-        print("trainsize {}, val size {}, test size {}".format(TRAIN_SIZE, VAL_SIZE, TEST_SIZE))
-        
-        # Each tweet convert to words and store in corpus
-        train_dataset = dataset[:TRAIN_SIZE]
-        val_dataset = dataset[TRAIN_SIZE: (TRAIN_SIZE+VAL_SIZE)]
-        test_dataset = dataset[(VAL_SIZE+ TRAIN_SIZE):]
-        print(len(train_dataset), len(val_dataset), len(test_dataset))
-        train_dataset
-
-        # train data and label
-        X_train = train_dataset[:, 0]
-        Y_train = train_dataset[:, 1]
-
-        # validation data and label
-        X_validation = val_dataset[:, 0]
-        Y_validation = val_dataset[:, 1]
-
-        # test data and label
-        X_test = test_dataset[:, 0]
-        Y_test =test_dataset[:, 1]
-
-        # Tokenizer
-        max_words = 5000
-        self.max_len = 100
-
-        self.tokenizer = Tokenizer(num_words=max_words, oov_token='<OOV>')
-        self.tokenizer.fit_on_texts(X_train)
-        sequences = self.tokenizer.texts_to_sequences(X_train)
-        X_train = pad_sequences(sequences, maxlen=self.max_len)
-        val_sequences = self.tokenizer.texts_to_sequences(X_validation)
-        X_validation = pad_sequences(val_sequences, maxlen=self.max_len)
-
-        # One hot encode labels
-        Y_train=np.asarray(Y_train).astype(np.int)
-        Y_validation = np.asarray(Y_validation).astype(np.int)
-
-        temp_train = []
-        for val in Y_train:
-          if val == 0:
-            temp_train.append([1, 0, 0])
-          if val == 1:
-            temp_train.append([0, 1, 0])
-          if val == 2:
-            temp_train.append([0, 0, 1])
-
-        Y_train = temp_train
-
-        temp_val = []
-        for val in Y_validation:
-          if val == 0:
-            temp_val.append([1, 0, 0])
-          if val == 1:
-            temp_val.append([0, 1, 0])
-          if val == 2:
-            temp_val.append([0, 0, 1])
-
-        Y_validation = temp_val
-
-        Y_train=np.asarray(Y_train).astype(np.int)
-        Y_validation = np.asarray(Y_validation).astype(np.int)
-        
         # Building Model and Train Model
         embedding_layer = Embedding(1000, 64)
         model = Sequential()
@@ -126,12 +125,12 @@ class SentimentModel:
         model.add(layers.Dense(3,activation='softmax'))
         model.compile(optimizer='rmsprop',loss='categorical_crossentropy', metrics=['accuracy'])
         checkpoint = ModelCheckpoint("best_model1.hdf5", monitor='val_accuracy', verbose=1,save_best_only=True, mode='auto', period=1,save_weights_only=False)
-        history = model.fit(X_train, Y_train, epochs=70,validation_data=(X_validation, Y_validation),callbacks=[checkpoint])
+        history = model.fit(X_train, Y_train, epochs=5,validation_data=(X_validation, Y_validation),callbacks=[checkpoint])
         return model
     
     def predict_sentiment(self, text_twitter):
         sentiment = ['Neutral','Positive','Negative']
-        sequence = self.tokenizer.texts_to_sequences([text_twitter])
-        test = pad_sequences(sequence, maxlen=self.max_len)
+        sequence = tokenizer.texts_to_sequences([text_twitter])
+        test = pad_sequences(sequence, maxlen=max_len)
         result = sentiment[np.around(self.model.predict(test), decimals=0).argmax(axis=1)[0]]
         return result
--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def predict_sentiment(sentiment: SentimentText):
     }
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=9000)
